78fb48e0b86d35700ab7423056197556962a009f
abhashksingh <superabhash@gmail.com>
2023-08-27 12:28:37 +0000

    - implementing a model

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 float train[][2] = {
 	{0,0},
@@ -8,6 +10,7 @@ float train[][2] = {
 	{3,6},
 	{4,8}
 };
+#define train_count (sizeof(train)/sizeof(train[0]))
 
 float rand_float(void){
 	return (float)rand()/(float)RAND_MAX;
@@ -15,8 +18,16 @@ float rand_float(void){
 
 int main()
 {
+	srand(time(0));
 	//y = x*w
-	float w = rand_float();
-	printf("%f\n",w);
+	float w = rand_float()*10;
+	
+	for(size_t i = 0; i<train_count;++i)
+	{
+		float x = train[i][0];
+		float y = x*w;
+		printf("actual:%f, expected:%f\n",y,train[i][1]);
+	}
+
 	return 0;
 }
