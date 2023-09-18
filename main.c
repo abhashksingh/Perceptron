ff7fb821685f9f29eb4d546dfaad1c8f131062ce
abhashksingh <superabhash@gmail.com>
2023-09-18 11:32:37 +0000

    - pushing

--- main.c
+++ main.c
@@ -16,20 +16,25 @@ float rand_float(void){
 	return (float)rand()/(float)RAND_MAX;
 }
 
+float cost(float w)
+{
+	for(size_t i = 0; i<train_count;++i)
+        {
+                float x = train[i][0];
+                float y = x*w;
+                float d = y-train[i][1];
+                result+=(d*d);
+                printf("actual:%f, expected:%f\n",y,train[i][1]);
+        }		
+}
+
+
 int main()
 {
 	srand(time(0));
 	//y = x*w
 	float w = rand_float()*10;
 	float result = 0.0f;
-	for(size_t i = 0; i<train_count;++i)
-	{
-		float x = train[i][0];
-		float y = x*w;
-		float d = y-train[i][1];
-		result+=(d*d);
-		printf("actual:%f, expected:%f\n",y,train[i][1]);
-	}
 
 	result/=train_count;
 	printf("%f\n",result);
